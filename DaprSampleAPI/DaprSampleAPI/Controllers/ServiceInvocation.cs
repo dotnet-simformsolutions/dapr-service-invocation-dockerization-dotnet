858f307651d99c8185fdc9f30d47043013d2da49
nileshprajapati744 <nilesh.p@simformsolutions.com>
2024-09-25 10:16:05 +0530

    Added services

---
+++ DaprSampleAPI/DaprSampleAPI/Controllers/ServiceInvocation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Dapr.SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceInvocation : ControllerBase
+    {
+        //string baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3501");
+        string baseURL = "http://host.docker.internal:3501";
+        [HttpGet]
+        [Route("saveOrder")]
+        public async Task<dynamic> SaveOrder(string value)
+        {
+
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            // Adding app id as part of the header
+            client.DefaultRequestHeaders.Add("dapr-app-id", "order-processor");
+
+            var order = new Order(value);
+            var orderJson = JsonSerializer.Serialize<Order>(order);
+            var content = new StringContent(orderJson, Encoding.UTF8, "application/json");
+
+            Console.WriteLine("Http Url : " + $"{baseURL}/orders");
+            // Invoking a service
+            var response = await client.PostAsync($"{baseURL}/orders", content);
+            return response;       
+        }
+
+        public record Order(
+            [property: JsonPropertyName("orderId")] string OrderId
+            );
+    }
+}
---
+++ DaprSampleAPI/Libs/Dapr.OrderProcessor/Controllers/OrderProcessorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dapr.OrderProcessor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderProcessorController : ControllerBase
+    {
+
+        [HttpPost("/orders")]
+        public string Post(Order order)
+        {
+            Console.WriteLine("Order received : " + order);
+            return order.ToString();
+        }
+    }
+
+    public record Order(string orderId);
+}
